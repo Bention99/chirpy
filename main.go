ca31bd318e714304daa7592878afd58c9b61f4d0
Bention99 <benoitwyss0@gmail.com>
2026-01-27 19:37:25 +0100

    go modules updated

--- main.go
+++ main.go
@@ -5,18 +5,31 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"os"
+	"database/sql"
+	"fmt"
+	"github.com/Bention99/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Error connection to the Database: %v", err)
+	}
+	dbQueries := database.New(db)
+
 	const filepathRoot = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db: dbQueries,
 	}
 
 	mux := http.NewServeMux()
